f1da069b3d3b48d39f5ffee2da16ebceaafce510
XeniaLS13 <kseniavagabova@mail.ru>
2023-01-12 03:47:45 +0300

    Добавлен код по трём заданиям, файл readme

---
+++ Task1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberA = Convert.ToString(number);
+Console.WriteLine("вторая цифра этого числа -> "+numberA[1]);
---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите цифру, соответствующую дню недели: ");
+int weekdayNumber = Convert.ToInt32(Console.ReadLine());
+
+void WeekDayChecking (int dayNumber) 
+{
+  if (weekdayNumber == 6 || weekdayNumber == 7) 
+    {
+  Console.WriteLine("(этот день выходной) -> да");
+    }
+  else if (weekdayNumber < 1 || weekdayNumber > 7) 
+    {
+    Console.WriteLine("это не день недели");
+    }
+  else Console.WriteLine("(это будний день) -> нет");
+}
+
+WeekDayChecking(weekdayNumber);
